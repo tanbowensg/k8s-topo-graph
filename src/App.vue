bee18cfe03c11257feee4b955fb11ed1a24574ec
tanbowen <tanbowensg@gmail.com>
2017-09-08 11:38:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="topo-graph-container">
+    <div id="dce-topo-graph">
+      6666
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="scss">
+// 拓扑图的大小是容器决定的
+#topo-graph-container {
+  height: 600px;
+  width: 800px;
+}
+#dce-topo-graph{
+  background: grey;
+  height: 100%;
+  width: 100%;
+}
+</style>
