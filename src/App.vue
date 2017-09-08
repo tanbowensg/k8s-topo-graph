838d528e02a4f39df1cd1362af1de4c4a2755f31
tanbowen <tanbowensg@gmail.com>
2017-09-08 16:59:40 +0800

    整体模板完成

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,57 @@
 <template>
   <div id="topo-graph-container">
     <div id="dce-topo-graph">
-      6666
+      <header id="dce-topo-graph-header">
+        <div id="app-name">ndc/my-app</div>
+        <ul id="pages">
+          <li class="page active">服务</li><!--
+       --><li class="page">网络</li><!--
+       --><li class="page">存储</li>
+        </ul>
+      </header>
+      <div id="dce-topo-graph-body">
+        <div id="graph-canvas">
+          <div class="topo-node"></div>
+          <div class="topo-node"></div>
+          <div class="topo-node"></div>
+        </div>
+        <div id="code-section">
+          <header id="code-header">
+            <div class="code-tab active">YAML 编排</div><!--
+          --><div class="code-tab">错误消息</div><!--
+          --><div class="code-tab">帮助文档</div>
+          </header>
+          <div id="code-main">
+          </div>
+          <footer id="code-footer">
+            <div class="footer-btn active">保存</div><!--
+         --><div class="footer-btn">取消</div>
+          </footer>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
+  // components: {
+  //   Hello
+  // }
 }
 </script>
 
 <style lang="scss">
-// 拓扑图的大小是容器决定的
-#topo-graph-container {
-  height: 600px;
-  width: 800px;
+@import './app.scss';
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-#dce-topo-graph{
-  background: grey;
-  height: 100%;
-  width: 100%;
+ul {
+  list-style: none;
 }
 </style>
