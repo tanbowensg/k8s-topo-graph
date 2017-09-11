afc9e4799c4d8fe15bb484e8177afb101c180f21
tanbowen <tanbowensg@gmail.com>
2017-09-11 17:21:11 +0800

    添加了 svg 划线

--- src/components/topo-canvas/topo-canvas.vue
+++ src/components/topo-canvas/topo-canvas.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="graph-canvas">
     <topo-node v-for="d in deployments" :key="d.name" :info="d"></topo-node>
+    <svg id="canvas-lines" xmlns="http://www.w3.org/2000/svg" version="1.1" class="viewport"
+         width="100%" height="100%">
+      <path :d="convertToSvgPath(120, 300, 400, 50)" stroke="#fff" stroke-width="1.1" opacity="0.6"
+      ></path>
+    </svg>
   </div>
 </template>
 
@@ -33,8 +38,13 @@ export default {
           ],
         };
       });
-    }
+    },
   },
+  methods: {
+    convertToSvgPath(x1, y1, x2, y2) {
+      return `M${x1},${y1}L${x2},${y2}`;
+    }
+  }
 }
 </script>
 
