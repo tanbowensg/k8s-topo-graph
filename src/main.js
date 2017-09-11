8ecf9c23d32da4fe5648863e993defe21dd6f3e4
tanbowen <tanbowensg@gmail.com>
2017-09-11 10:59:31 +0800

    引入 RxJs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './app'
+import Rx from 'rxjs/Rx';
+import VueRx from 'vue-rx';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRx, Rx);
 
 /* eslint-disable no-new */
 new Vue({
