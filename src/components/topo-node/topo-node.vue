4e2cd30bae82887bc7689cd2149629e10e0c4db7
tanbowen <tanbowensg@gmail.com>
2017-09-12 14:28:01 +0800

    优化了拖拽体验

--- src/components/topo-node/topo-node.vue
+++ src/components/topo-node/topo-node.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="topo-node"
+    ref="topoNode"
     :style="{transform: transformValue}"
-    v-stream:mousedown="mousedown$"
-    v-stream:mouseup="mouseup$">
+    v-stream:mousedown="mousedown$">
     <header class="topo-node-header">
       <div class="pivot left" v-if="info.hasDependency"></div>
       <div class="pivot right" v-if="info.isDependency"></div>
@@ -18,15 +18,16 @@
 </template>
 
 <script>
-
 export default {
   name: 'TopoNode',
   props: ['info'],
-  domStreams: ['mousedown$', 'mouseup$'],
+  domStreams: ['mousedown$'],
   data(){
     return {
       x: 0,
       y: 0,
+      maxX: Infinity,
+      maxY: Infinity,
     };
   },
   computed: {
@@ -34,19 +35,44 @@ export default {
       return `translate(${this.x}px, ${this.y}px)`
     }
   },
+  mounted() {
+    // 最大 X 和 Y就是节点可以拖动的范围上限
+    // 注意：额外减 2 的是阴影
+    this.maxX = this.$refs.topoNode.parentElement.clientWidth - this.$refs.topoNode.clientWidth - 2;
+    this.maxY = this.$refs.topoNode.parentElement.clientHeight - this.$refs.topoNode.clientHeight - 2;
+  },
   subscriptions() {
     // {x: xxx, y: xxx}
     const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove');
+    const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
     const mouseDrag$ = this.mousedown$.switchMap(() => {
-      return mousemove$.takeUntil(this.mouseup$)
+      return mousemove$.takeUntil(mouseup$)
         .map(event => {
           return {x: event.movementX, y: event.movementY}
         })
     })
+    .scan((acc, {x, y}) => {
+      let finalX, finalY;
+      const newX = acc.x + x;
+      const newY = acc.y + y;
+        // 以免超出上限和下限
+      if (newX < 0) finalX = 0;
+      else if (newX > this.maxX) finalX = this.maxX;
+      else finalX = newX;
 
-    this.$subscribeTo(mouseDrag$, movement => {
-      this.x = this.x + movement.x;
-      this.y = this.y + movement.y;
+      if (newY < 0) finalY = 0;
+      else if (newY > this.maxY) finalY = this.maxY;
+      else finalY = newY;
+
+      return {
+        x: finalX,
+        y: finalY,
+      }
+    }, {x: 0, y: 0})
+
+    this.$subscribeTo(mouseDrag$, position => {
+      this.x = position.x;
+      this.y = position.y;
       this.emitMovement();
     })
   },
